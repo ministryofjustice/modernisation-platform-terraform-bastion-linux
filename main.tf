53ae24e9db08b273dfe63511fbc725949d8c188b
George Fountopoulos <george.fountopoulos@digital.justice.gov.uk>
2021-09-07 15:44:19 +0100

    Resolve linting issues

--- main.tf
+++ main.tf
@@ -130,6 +130,15 @@ resource "aws_s3_bucket" "replica" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "replica" {
+  bucket = aws_s3_bucket.replica.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket" "default" {
   bucket = "${var.tags_prefix}-${var.bucket_name}"
   acl    = "private"
