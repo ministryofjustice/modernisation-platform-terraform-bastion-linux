bbe2f2612439afe66ab5f3af138703778de1f570
George Fountopoulos <george.fountopoulos@digital.justice.gov.uk>
2021-10-29 14:17:55 +0100

    Error: Reference to undeclared resource
      on .terraform/modules/bastion_linux/outputs.tf line 3, in output "bastion_private_ip":
       3:   value       = aws_instance.bastion_linux.private_ip
    
    A managed resource "aws_instance" "bastion_linux" has not been declared in
    module.bastion_linux.
    Error: Process completed with exit code 1.

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,10 @@
+data "aws_instances" "ecs_instances_meta" {
+  instance_tags = {
+    Name = "bastion_linux"
+  }
+}
+
 output "bastion_private_ip" {
   description = "Private IP of bastion"
-  value       = aws_instance.bastion_linux.private_ip
+  value       = data.aws_instances.ecs_instances_meta.private_ips
 }
