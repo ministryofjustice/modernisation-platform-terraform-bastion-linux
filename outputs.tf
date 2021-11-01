7deefe98d8c62e0fb6b0522ddbf0d4a3c6c3b32e
George Fountopoulos <george.fountopoulos@digital.justice.gov.uk>
2021-11-01 09:59:02 +0000

    output security group instead of private_ip

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,4 @@
-data "aws_instances" "ecs_instances_meta" {
-  depends_on    = [aws_autoscaling_group.bastion_linux_daily]
-  instance_tags = {
-    Name = "bastion_linux"
-  }
-}
-
-output "bastion_private_ip" {
-  description = "Private IP of bastion"
-  value       = data.aws_instances.ecs_instances_meta.private_ips[0]
+output "bastion_security_group" {
+  description = "Security group of bastion"
+  value       = aws_security_group.bastion_linux.id
 }
