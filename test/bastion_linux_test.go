ad700499543d9a8a22832ffa57dd452a99cdead1
George Fountopoulos <george.fountopoulos@digital.justice.gov.uk>
2022-06-15 16:45:41 +0100

    checking launch template and S3 bucket

--- test/bastion_linux_test.go
+++ test/bastion_linux_test.go
@@ -21,6 +21,15 @@ func TestS3Creation(t *testing.T) {
 	terraform.InitAndApply(t, terraformOptions)
 
 	bastionSecurityGroup := terraform.Output(t, terraformOptions, "bastion_security_group")
+	bastionLaunchTemplate := terraform.Output(t, terraformOptions, "bastion_launch_template")
+	bastionS3Bucket := terraform.Output(t, terraformOptions, "bastion_s3_bucket")
 
 	assert.Regexp(t, regexp.MustCompile(`^sg-*`), bastionSecurityGroup)
+
+	assert.Regexp(t, regexp.MustCompile(`^sg-*`), bastionLaunchTemplate.image_id)
+	assert.Equal(t, "terminate", bastionLaunchTemplate.instance_initiated_shutdown_behavior)
+	assert.Equal(t, "t3.micro"), bastionLaunchTemplate.instance_type)
+
+	assert.Regexp(t, regexp.MustCompile(`^arn:aws:s3:::s3-bucket-*`), bastionS3Bucket.bucketArn)
+	assert.Regexp(t, regexp.MustCompile(`^s3-bucket-*`), bastionS3Bucket.bucketName)
 }
--- test/unit-test/outputs.tf
+++ test/unit-test/outputs.tf
@@ -2,3 +2,13 @@ output "bastion_security_group" {
   value       = module.bastion_linux.bastion_security_group
   description = "Security group of bastion"
 }
+
+output "bastion_launch_template" {
+  description = "Launch template of bastion"
+  value       = module.bastion_linux.bastion_launch_template
+}
+
+output "bastion_s3_bucket" {
+  description = "S3 bucket of bastion"
+  value       = module.bastion_linux.bastion_s3_bucket
+}
