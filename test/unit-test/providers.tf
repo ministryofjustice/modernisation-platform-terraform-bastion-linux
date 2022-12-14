441e9a0bba76ccc28b745cff3de7b4f462cf90e1
David Elliott <david.elliott@digital.justice.gov.uk>
2022-12-14 09:11:19 +0000

    Remove deprecated provider attribute

--- test/unit-test/providers.tf
+++ test/unit-test/providers.tf
@@ -9,16 +9,14 @@ provider "aws" {
 
 # AWS provider for the testing-ci user (testing-test account), to get things from there if required
 provider "aws" {
-  alias                  = "testing-ci-user"
-  region                 = "eu-west-2"
-  skip_get_ec2_platforms = true
+  alias  = "testing-ci-user"
+  region = "eu-west-2"
 }
 
 # AWS provider for core-vpc-<environment>, to share VPCs into this account
 provider "aws" {
-  alias                  = "core-vpc"
-  region                 = "eu-west-2"
-  skip_get_ec2_platforms = true
+  alias  = "core-vpc"
+  region = "eu-west-2"
   assume_role {
     role_arn = "arn:aws:iam::${local.environment_management.account_ids[local.provider_name]}:role/member-delegation-${local.vpc_name}-${local.environment}"
   }
@@ -26,9 +24,8 @@ provider "aws" {
 
 # AWS provider for network services to enable dns entries for certificate validation to be created
 provider "aws" {
-  alias                  = "core-network-services"
-  region                 = "eu-west-2"
-  skip_get_ec2_platforms = true
+  alias  = "core-network-services"
+  region = "eu-west-2"
   assume_role {
     role_arn = "arn:aws:iam::${local.environment_management.account_ids["core-network-services-production"]}:role/modify-dns-records"
   }
